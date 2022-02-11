a3fdead72d801a5377579c8fe28653ae9718ada2
zlc <1835242336@qq.com>
2022-02-11 20:10:36 +0800

    写好结构

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+        <header>
+            <div class="topbar"></div>
+            <div class="header"></div>
+        </header>
+        <div class="home-container container">
+
+        </div>
+        <div class="main container">
+
+        </div>
+        <footer>
+            
+        </footer>
+</template>
